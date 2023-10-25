5fa870ed90bbb489a6f7730fdc738ce7e1138e67
Serafin Quesada <quesada.serafin03@gmail.com>
2023-10-24 23:17:19 -0300

    build forms login and register with validation

--- frontend/src/rules/index.tsx
+++ frontend/src/rules/index.tsx
@@ -8,21 +8,24 @@ export const schemaLogin = yup.object().shape({
 export const schemaRegister = yup.object().shape({
     username: yup
         .string()
-        .min(5, "At least should be five characters short")
-        .required("Please enter an username"),
+        .required("Please enter an username")
+        .min(5, "At least should be five characters short"),
     email: yup
         .string()
-        .required("Please enter a valid email")
-        .matches(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "Please enter a valid email"),
+        .required("Please enter an email")
+        .matches(
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            , "Please enter a valid email"),
     password: yup
         .string()
         .required('Please enter your password')
-        .matches(
-            /^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$/,
-            "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
-        ),
+        // .matches(
+        //     /^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$/,
+        //     "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+        // )
+        ,
     repeatPassword: yup
         .string()
-        .required('Passwords must match')
+        .required('Please re-enter your password')
         .oneOf([yup.ref('password')], 'Passwords must match')
 })

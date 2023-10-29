8d1e6f5046d7b5319ab95ec060877d1f894f2b14
NachoJ12 <nacho.justel12@gmail.com>
2023-10-29 02:35:55 -0300

    feat(ms-user): Add user and role entities

---
+++ backend/ms-user/src/main/java/com/passportal/msuser/entity/User.java
@@ -0,0 +1,37 @@
+package com.passportal.msuser.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    @Column(name = "mail")
+    private String email;
+    private String password;
+    private String name;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "last_pass_reset")
+    private LocalDateTime lastPassReset;
+    private boolean enabled;
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @JoinColumn(name = "role_id",referencedColumnName = "id")
+    private Role role;
+
+}

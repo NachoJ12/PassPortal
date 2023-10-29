3807ee79509434589cc185a5a5e032993acfef36
NachoJ12 <nacho.justel12@gmail.com>
2023-10-29 01:03:54 -0300

    feat(ms-discovery): Add ms-discovery to the project

---
+++ backend/ms-discovery/src/test/java/com/passportal/msdiscovery/MsDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passportal.msdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

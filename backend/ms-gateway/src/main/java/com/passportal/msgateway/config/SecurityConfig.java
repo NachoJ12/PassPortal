006706f3d586a81f9b010f8e68aee46e92a5ace9
NachoJ12 <nacho.justel12@gmail.com>
2023-10-29 01:53:56 -0300

    feat(ms-gateway): Add ms-gateway configuration and SecurityConfig
    - Include the configuration of ms-gateway with Keycloak for enhanced security.
    - Use environment variables for sensitive data.
    - The SecurityConfig class has been implemented to set up OAuth2 authentication with Keycloak, allowing public access to the '/users/create' endpoint while securing other endpoints.

---
+++ backend/ms-gateway/src/main/java/com/passportal/msgateway/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.passportal.msgateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Value("${jwt-decoder-url}")
+    private String jwtDecoderUrl;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain (ServerHttpSecurity http){
+        http.cors().and().csrf().disable();
+        http
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtDecoder(jwtDecoder())
+                        )
+                )
+                .authorizeExchange()
+                .pathMatchers("/users/create").permitAll()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2Login(); //Check if this login needs to be present and which endpoint we hit to generate the token.
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder(){
+        return NimbusReactiveJwtDecoder.withJwkSetUri(jwtDecoderUrl).build();
+    }
+
+}

17949239016f664359552d64d6efd98b62b8021f
NachoJ12 <nacho.justel12@gmail.com>
2023-11-02 00:14:08 -0300

    feat(ms-gateway): Add OAuth2 login and logout configuration

--- backend/ms-gateway/src/main/java/com/passportal/msgateway/config/SecurityConfig.java
+++ backend/ms-gateway/src/main/java/com/passportal/msgateway/config/SecurityConfig.java
@@ -28,7 +28,9 @@ public class SecurityConfig {
                 .anyExchange()
                 .authenticated()
                 .and()
-                .oauth2Login(); //Check if this login needs to be present and which endpoint we hit to generate the token.
+                .oauth2Login()
+                .and()
+                .logout();
         return http.build();
     }
 

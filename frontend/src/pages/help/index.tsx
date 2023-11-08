8a8c01c52c7d424a19f76675563e83dda295a5e4
NachoJ12 <nacho.justel12@gmail.com>
2023-11-07 22:04:36 -0300

    Fix build

--- frontend/src/pages/help/index.tsx
+++ frontend/src/pages/help/index.tsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import type { NextPage, GetStaticProps } from 'next'
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
+import React from 'react';
+import type { NextPage, GetStaticProps } from 'next';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import BaseLayout from '@/components/layouts/base-layout'
-import { Faqs } from '@/data/faqs';
+import BaseLayout from '@/components/layouts/base-layout';
+import { Faqs, faqs } from '@/data/faqs';
 import { getFaqs } from '@/service/faqs-service';
-import {  ThemeProvider, createTheme } from '@mui/system';
+import { ThemeProvider, createTheme } from '@mui/system';
 
 interface Props {
-  faqs: Faqs[]
+  faqs: Faqs[];
 }
 
-
-
-const Help: NextPage<Props> = ({ faqs }) => {
+const Help = () => {
   return (
     <BaseLayout>
       <ul>
         {faqs.map((faq) => (
-          <Accordion sx={{backgroundColor: "#070607", color:"#9d9c9d", width:"80%", alignSelf:"center"}} key={faq.id} >
+          <Accordion
+            sx={{
+              backgroundColor: '#070607',
+              color: '#9d9c9d',
+              width: '80%',
+              alignSelf: 'center',
+            }}
+            key={faq.id}
+          >
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
               aria-controls="panel1a-content"
@@ -27,22 +38,13 @@ const Help: NextPage<Props> = ({ faqs }) => {
               <Typography>{faq.question}</Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <Typography>
-              {faq.answer}
-              </Typography>
+              <Typography>{faq.answer}</Typography>
             </AccordionDetails>
           </Accordion>
         ))}
       </ul>
     </BaseLayout>
-  )
-}
-
-
+  );
+};
 
 export default Help;
-
-export const getStaticProps: GetStaticProps = async () => {
-  const faqs = await getFaqs()
-  return { props: { faqs } }
-}

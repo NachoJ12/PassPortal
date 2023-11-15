e5028481e862f8f00b0cbd55761c5c659b77bb73
Serafin Quesada <quesada.serafin03@gmail.com>
2023-11-15 15:52:03 -0300

    improving faqs page

--- frontend/src/pages/help/index.tsx
+++ frontend/src/pages/help/index.tsx
@@ -10,6 +10,7 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import BaseLayout from '@/components/layouts/base-layout';
 import { Faqs, faqs } from '@/data/faqs';
 import { getFaqs } from '@/service/faqs-service';
+import Link from 'next/link';
 
 interface Props {
   faqs: Faqs[];
@@ -19,27 +20,42 @@ const Help: NextPage<Props> = () => {
   return (
     <BaseLayout>
       <div className='help-page'>
-        <ul className='faqs_list'>
-          {faqs?.map((faq) => (
-            <Accordion sx={{ backgroundColor: "#070607", color: "#9d9c9d", width: "80%", alignSelf: "center" }} key={faq.id} >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+        <h1 style={{ textAlign: "center", marginTop:"3rem", color: "#9d9c9d",}}>
+        Frequently Ask Questions
+      </h1>
+      <ul className='faqs_list'>
+        {faqs?.map((faq) => (
+          <Accordion sx={{ backgroundColor: "#070607", color: "#9d9c9d", width: "80%", alignSelf: "center" }} key={faq.id} >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>{faq.question}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                {faq.answer}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </ul>
+      <div>
+        <h4  style={{ textAlign: "center", marginTop:"3rem", color: "#9d9c9d",}}>
+          More Doubts?  
+          <span style={{color: "#9d9c9d"}}>
+            <Link
+              href="/contact"
               >
-                <Typography>{faq.question}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  {faq.answer}
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-          ))}
-        </ul>
+              Contact Us
+            </Link>
+          </span>
+        </h4>
       </div>
+    </div>
 
-    </BaseLayout>
+    </BaseLayout >
   )
 }
 

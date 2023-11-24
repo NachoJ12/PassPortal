60176d16cf927715ca255b64cc823192e6ff74ea
flaviobernachia <flaviobernachia@gmail.com>
2023-11-24 17:19:03 -0300

    create cardProfile, and styles

--- frontend/src/components/layouts/navbar.tsx
+++ frontend/src/components/layouts/navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState } from 'react'
 import { signOut, useSession } from "next-auth/react";
 import Link from 'next/link'
 
@@ -15,15 +15,18 @@ const navbarItems = [
   }
 ]
 
-export default function navbar() {
+interface NavbarProps {
+  onUsernameClick: () => void;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ onUsernameClick }) => {
   const { data: session } = useSession();
 
   return (
     <nav className='navbar'>
-
       {session?.user ? (
         <>
-          <span className="navbar_link" >
+          <span className="navbar_link" onClick={onUsernameClick}>
             {session.user.name}
           </span>
           <Link
@@ -36,15 +39,17 @@ export default function navbar() {
         </>
       ) : (
         <ul className='navbar_list'>
-          {navbarItems.map(item =>
-            <li className='navbar_item' key={item.name}>
-              <Link className="navbar_link" href={item.href} >
-                <span className='sidebar_name'> {item.name}</span>
+          {navbarItems.map((item) => (
+            <li className="navbar_item" key={item.name}>
+              <Link className="navbar_link" href={item.href}>
+                <span className="sidebar_name"> {item.name}</span>
               </Link>
             </li>
-          )}
+          ))}
         </ul>
       )}
     </nav>
-  )
-}
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,20 +1,18 @@
-import BaseLayout from "@/components/layouts/base-layout";
-import type { NextPage } from "next";
-import CardUpcomingContainer from "@/components/ui/cardGeneral/cardUpcoming/cardUpcomingContainer/cardUpcomingContainer";
-import { cardItems, } from "@/data/cardItems";
-import { CardShowsContainer } from "@/components/ui/cardGeneral/cardShows/cardShowsContainer/cardShowsContainer";
-import CarouselImage from "@/components/ui/carousel/carousel";
-import CarouselCard from "@/components/ui/carousel/carouselCardInfo";
-import Image from "next/image";
-import passPortalLogo from "../../public/logo-grey.svg";
-import { Card, Typography, CardActions, Button } from "@mui/material";
-import { useRouter } from 'next/navigation'
+import React from 'react';
+import BaseLayout from '@/components/layouts/base-layout';
+import CardUpcomingContainer from '@/components/ui/cardGeneral/cardUpcoming/cardUpcomingContainer/cardUpcomingContainer';
+import { cardItems } from '@/data/cardItems';
+import { CardShowsContainer } from '@/components/ui/cardGeneral/cardShows/cardShowsContainer/cardShowsContainer';
+import CarouselImage from '@/components/ui/carousel/carousel';
+import CarouselCard from '@/components/ui/carousel/carouselCardInfo';
+import Image from 'next/image';
+import passPortalLogo from '../../public/logo-grey.svg';
+import { Card, Typography, CardActions, Button } from '@mui/material';
+import { useRouter } from 'next/router';
+import { NextPage } from 'next/types';
 
 const Home: NextPage = () => {
-
-  const router = useRouter()
-
-
+  const router = useRouter();
 
   return (
     <BaseLayout>
@@ -27,20 +25,11 @@ const Home: NextPage = () => {
           </div>
           <div className="upcoming-section">
             <Card className="subcard-Upcoming" data-aos="fade-up">
-              <Typography
-                variant="h6"
-                component="div"
-                className="subcard-Upcoming-typepography"
-              >
+              <Typography variant="h6" component="div" className="subcard-Upcoming-typepography">
                 Upcoming Events
               </Typography>
               <CardActions>
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  className="boton-Upcoming"
-                  onClick={()=>  router.push(`events`)}
-                >
+                <Button variant="outlined" color="primary" className="boton-Upcoming" onClick={() => router.push(`events`)}>
                   Show More
                 </Button>
               </CardActions>
@@ -49,8 +38,8 @@ const Home: NextPage = () => {
           </div>
         </div>
         <div className="cardShow-main-container">
-        <h2 className="cardShow-main-title">Show And Events</h2>
-        <CardShowsContainer events={cardItems} />
+          <h2 className="cardShow-main-title">Show And Events</h2>
+          <CardShowsContainer events={cardItems} />
         </div>
       </div>
     </BaseLayout>

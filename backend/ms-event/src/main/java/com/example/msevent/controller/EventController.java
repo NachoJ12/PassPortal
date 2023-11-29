956530b939c1771048ff39ea455154fdb1bdd0c2
NachoJ12 <nacho.justel12@gmail.com>
2023-11-28 23:15:22 -0300

    fix(ms-event): fix endpoint and add config server

--- backend/ms-event/src/main/java/com/example/msevent/controller/EventController.java
+++ backend/ms-event/src/main/java/com/example/msevent/controller/EventController.java
@@ -16,7 +16,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Controller
-@RequestMapping("/event")
+@RequestMapping("/events")
 @RequiredArgsConstructor
 public class EventController {
 

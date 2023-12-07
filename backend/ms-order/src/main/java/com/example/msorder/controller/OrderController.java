5ff6ff48e2f400dd557690ca541fc468baabe4a4
NachoJ12 <nacho.justel12@gmail.com>
2023-12-06 21:32:02 -0300

    chore(ms-order): change anottation @Controller to @RestController

--- backend/ms-order/src/main/java/com/example/msorder/controller/OrderController.java
+++ backend/ms-order/src/main/java/com/example/msorder/controller/OrderController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,7 +17,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/order")
 @RequiredArgsConstructor
 public class OrderController {
--- backend/ms-order/src/main/java/com/example/msorder/controller/TicketController.java
+++ backend/ms-order/src/main/java/com/example/msorder/controller/TicketController.java
@@ -4,13 +4,12 @@ import com.example.msorder.model.Ticket;
 import com.example.msorder.service.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/ticket")
 public class TicketController {
